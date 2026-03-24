97192cd122fa035275d1c1c9c6e674c6dd98150a
Rahul Yadav <A2zautomation@outlook.com>
2026-03-24 18:00:23 +0530

    test123

--- src/testproject/Main.java
+++ src/testproject/Main.java
@@ -4,7 +4,7 @@ public class Main {
 	public static void main(String[] args) {
         System.out.println("Hello Automation Rahul sikayaega");
         
-        Now i can do it 
+        I will fuck the world in next 3 days 
         
         
 }
