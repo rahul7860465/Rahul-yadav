32d7879e4f9f22ab7c373ca195874ea6b32440cc
Rahul Yadav <A2zautomation@outlook.com>
2026-03-24 17:49:52 +0530

    test bash

--- src/testproject/Main.java
+++ src/testproject/Main.java
@@ -4,5 +4,10 @@ public class Main {
 	public static void main(String[] args) {
         System.out.println("Hello Automation Rahul sikayaega");
         
+        
+        int = int1 new 123;
+        
+        make new chnages 
+        
 }
 }
